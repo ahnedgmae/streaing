e03df333c4e15427009ca7895b15a46db65470fa
ahnedgmae <49765614+ahnedgmae@users.noreply.github.com>
2019-06-17 11:31:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "!";
 var adminprefix = '#'
 
 
-const developers = ["405529828058595328","ID"]
+const developers = ["405529828058595328"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
